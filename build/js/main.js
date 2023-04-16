5f77423e49a5c56d206477ec67a5646f67555e82
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-04-16 21:23:30 +0300

    Objects,Arrays, Enums, Tuples

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,60 @@
 "use strict";
-let myName = "Dave";
-let meaningOfLife;
-let isLoading;
-let album;
-myName = "ilias";
-meaningOfLife = 42;
-isLoading = false;
-album = "van hallen";
-album = 42;
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ["one", "hey", "ilias"];
+let quitars = ["strat", "les Paul", 5150]; //union
+let mixedData = ["EVH", 1984, true];
+stringArr[0] = "John";
+stringArr.push("ilias");
+quitars[0] = 1984;
+quitars.unshift("Jim");
+quitars = stringArr;
+let test = [];
+let bands = [];
+bands.push("lsfsd");
+//Tuple - length and types specific
+let myTuple = ["Dave", 42, true];
+let mixed = ["John", 1, false];
+//mixed = myTuple -->its ok
+//myTuple = mixed //its not accepted because the source array (mixed) does not require specifically 3 elements only.Because later in the programm we may change the value of mixed so we will have a provblem.
+//myTuple[3] = 42 no!
+//OBJECTS
+let myObj;
+myObj = [];
+console.log(typeof myObj); // object be ause arrays = objects in js
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Dave",
+    prop2: true,
 };
-console.log(sum(5, "6"));
-let postId; // union type
-let isActive;
-let re = /\w+/g;
+exampleObj.prop2 = false; //cannot accept other types only boolean.
+let evh = {
+    name: "ilias",
+    active: true,
+    albums: [1984, 5150, "kenfsk"],
+};
+let jp = {
+    name: "George",
+    active: true,
+    albums: ["ser", "seser", "kenfsk"],
+};
+evh = jp;
+//evh.years = 40 //cannot
+//how to make a property optional -- we put ?at the properties before column
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}`;
+    }
+    return "Hello";
+};
+console.log(greetGuitarist(jp));
+//Enums
+// "Unlike" most Typescript features, Enums are not a type-level addition to javascript btu something addes to the language and runtime.
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 0] = "U";
+    Grade[Grade["D"] = 1] = "D";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["A"] = 4] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.D); // 1 by default
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,100 @@
-let myName: string = "Dave";
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArr = ["one", "hey", "ilias"];
 
-myName = "ilias";
-meaningOfLife = 42;
-isLoading = false;
-album = "van hallen";
-album = 42;
+let quitars = ["strat", "les Paul", 5150]; //union
 
-const sum = (a: number, b: string) => {
-  return a + b;
+let mixedData = ["EVH", 1984, true];
+
+stringArr[0] = "John";
+stringArr.push("ilias");
+
+quitars[0] = 1984;
+quitars.unshift("Jim");
+
+quitars = stringArr;
+
+let test = [];
+let bands: string[] = [];
+
+bands.push("lsfsd");
+
+//Tuple - length and types specific
+
+let myTuple: [string, number, boolean] = ["Dave", 42, true];
+
+let mixed = ["John", 1, false];
+
+//mixed = myTuple -->its ok
+
+//myTuple = mixed //its not accepted because the source array (mixed) does not require specifically 3 elements only.Because later in the programm we may change the value of mixed so we will have a provblem.
+
+//myTuple[3] = 42 no!
+
+//OBJECTS
+let myObj: object;
+myObj = [];
+
+console.log(typeof myObj); // object be ause arrays = objects in js
+
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Dave",
+  prop2: true,
 };
 
-console.log(sum(5, "6"));
+exampleObj.prop2 = false; //cannot accept other types only boolean.
 
-let postId: string | number; // union type
-let isActive: number | boolean;
+// type Guitarist = {
+//   name: string;
+//   active?: boolean;
+//   albums: (string | number)[];
+// };
 
-let re: RegExp = /\w+/g;
+//interface is more like a class
+
+interface Guitarist {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+}
+
+let evh: Guitarist = {
+  name: "ilias",
+  active: true,
+  albums: [1984, 5150, "kenfsk"],
+};
+let jp: Guitarist = {
+  name: "George",
+  active: true,
+  albums: ["ser", "seser", "kenfsk"],
+};
+
+evh = jp;
+
+//evh.years = 40 //cannot
+
+//how to make a property optional -- we put ?at the properties before column
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello ${guitarist.name.toUpperCase()}`;
+  }
+  return "Hello";
+};
+
+console.log(greetGuitarist(jp));
+
+//Enums
+// "Unlike" most Typescript features, Enums are not a type-level addition to javascript btu something addes to the language and runtime.
+
+enum Grade {
+
+    U,
+    D,
+    C,
+    B,
+    A
+}
+
+console.log(Grade.D) // 1 by default
